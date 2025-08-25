d6eead4607a7549479c733290edd33a6c228ea36
DESKTOP-IJLO8PN\Admin <nguyenhoangbaoha10a1@gmail.com>
2025-08-25 11:20:40 +0700

    Update

---
+++ backend/InteriorShop/InteriorShop.Infrastructure/Identity/ApplicationRole.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace InteriorShop.Infrastructure.Identity
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        public string? Description { get; set; }
+
+        public ApplicationRole() : base()
+        {
+
+        }
+
+        public ApplicationRole(string roleName, string? description = null) : base()
+        {
+            Description = description;
+        }
+    }
+}
---
+++ backend/InteriorShop/InteriorShop.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using InteriorShop.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace InteriorShop.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+    }
+}
